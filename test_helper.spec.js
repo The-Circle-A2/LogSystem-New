7fcda68043bf5d1b0940718c630b56ef40db31f8
mrcotrmpr <marco1088@hotmail.nl>
2021-06-21 10:26:58 +0200

    set up tests

---
+++ test_helper.spec.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const Log = require('./models/log.model');
+const testdb = process.env.DB_TEST;
+
+before(done => {
+    mongoose.connect(`mongodb+srv://${user}:${pass}@cluster0.6wbc5.mongodb.net/${testdb}?retryWrites=true&w=majority`);
+    mongoose.connection
+     .once('open', () => done())
+     .on('error', err => {
+         console.log('Warning', err);
+     });
+});
+
+afterEach(async () => {
+    await Promise.all([Log.deleteMany()])
+});
+
+
