9ca4b3103cf86175b2c2c504cc4d4efd47116df7
mrcotrmpr <marco1088@hotmail.nl>
2021-06-22 09:39:48 +0200

    set up integrity structure

---
+++ utils/rsaIntegrityHandler.js
@@ -0,0 +1,15 @@
+const JSEncrypt = require('JSEncrypt/bin/jsencrypt');
+const CryptoJS = require("crypto-js");
+
+function verifyLog(msg){
+
+}
+
+function signLog(msg){
+
+}
+
+module.exports = {
+    verifyLog,
+    signLog
+};
