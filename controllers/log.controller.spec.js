3217d290434af39e17cd4d4b92d111c9d9dcb7c4
mrcotrmpr <marco1088@hotmail.nl>
2021-06-22 11:06:18 +0200

    fixed test

--- controllers/log.controller.spec.js
+++ controllers/log.controller.spec.js
@@ -8,10 +8,10 @@ describe('log endpoints', function() {
 
     describe('integration tests', function() {
 
-        it('(POST /api/log) should create a log', async function() {
+        it('(POST /api/log) with valid signature should create a log', async function() {
             const testLog = {
-                message: 'Log message',
-                userName: 'Username'
+                message: 'log message over http',
+                signature: 'P+tW7Nd4t4T9QogKdXrGpOEuiwHbu/Biu8WHYUNompemz6cwo5y3gJbY0ln/AJx5Muv9oyqfKAboD9pHZqtPc1HdmprWsdUjZQS4to0wMnRKBolBezQA5jRc/tMsOAgJL+ttWupnG9PmHJ8GRI+gENkWum2+Do/869yjbA+P2i5fGRCXYQjD6jvdsMd23DVzS0MUcvO9kj8paG6P5iFlvco6PhApJhg5KpLDTvaKBhYWeluxlbWpuszxSiQVN+xMEc13mm8pSxGxTaMa06LMSJx8Kbv1Rhbf9y60yj3zvmCtvhu9vkWi6YKeE8bGxHdNUrO4UDcXB36721jA0NoCGg'
             };
 
             const res = await requester.post('/api/log').send(testLog);
